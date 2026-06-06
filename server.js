570cead19b26fa021247a24ea08af77c66339ecc
ben_jaccaci <jaccaci.b@northeastern.edu>
2026-06-06 16:56:45 -0400

    basic controller-server-view setup

--- server.js
+++ server.js
@@ -1,36 +1,87 @@
-"use strict";
+import http from "http";
+import fs from "fs";
+import { WebSocketServer } from "ws";
+const PORT = 1250;
 
-var { Server } = require("node-osc");
-var { Tone } = require("tone");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
 
-// Create a server listening on port 3333, bound to all interfaces
-var oscServer = new Server(3333, "0.0.0.0");
+  let filePath = "";
 
+  // Based on the request URL, determine which of the 2 html files to serve (controller client or campfire)
+  if (req.url === "/" || req.url === "/client" || req.url === "public/client") {
+    filePath = "public/client.html";
+  } else if (req.url === "/campfire" || req.url === "public/campfire") {
+    filePath = "public/campfire.html";
+  } else if (req.url === "/favicon.ico") {
+    res.statusCode = 204;
+    return res.end();
+  } else {
+    res.statusCode = 404;
+    filePath = "public/404.html";
+  }
 
-// Start a clock with the lowest possible latency
-// Prototype v1 doesn't need a clock or synchronization
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("Error loading file");
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    }
+  });
+});
 
-// Listen for incoming OSC messages
-oscServer.on("message", function (msg, rinfo) {
-  // msg is an array: [address, ...arguments]
-  console.log(`Message: ${msg}\nReceived from: ${rinfo.address}:${rinfo.port}`);
+const wss = new WebSocketServer({ server });
 
-  const osc = new Tone.Oscillator(msg[0], "sine").toDestination().start();
+let campfire = null;
+const musicians = new Map();
+let musicianIdCounter = 1;
 
-  const button = document.getElementById("play-button");
+function broadcastToCampfire(obj) {
+  const data = JSON.stringify(obj);
+  if (campfire) campfire.send(data);
+}
 
-  document.querySelector("button")?.addEventListener("click", async () => {
-    await Tone.start();
-    console.log("audio is ready");
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Received: ${message}`);
+
+    ws.id = musicianIdCounter++;
+
+    let parsedMsg = JSON.parse(message);
+
+    const address = parsedMsg.address;
+    const args = parsedMsg.args;
+
+    if (address === "/hello") {
+      if (args[0] === "campfire") {
+        campfire = ws;
+        console.log("Campfire client connected");
+      }
+    }
+
+    if (address === "/join") {
+      musicians.set(ws.id, args[0]);
+      console.log(`Musician ${ws.id} connected`);
+    }
+
+    if (address === "/note") {
+      broadcastToCampfire({ address: "/note", args });
+    }
   });
 
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    if (musicians.has(ws.id)) {
+      console.log(`Musician ${ws.id} leaving the campfire`);
+      musicians.delete(ws.id);
+    }
+  });
+});
 
-  // Listen for any messages from any clients
-  // For each message recieved:
-  // Add the message to a queue of messages to be quantized
-  // Quantize the message to the nearest # note
-  // Once the message is quantized, add it to a queue of messages to be sent to the fireplace client
-
-  // Close the server after receiving one message (for demo purposes)
-  oscServer.close();
+server.listen(PORT, () => {
+  console.log(`Node server running at http://localhost:${PORT}/`);
 });
