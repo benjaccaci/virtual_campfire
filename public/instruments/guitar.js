62fb827f4671a2ea6932bf9940618331075b248a
Amelie Pollack <148931343+ameliespl@users.noreply.github.com>
2026-06-11 09:11:06 -0400

    Update guitar.js

--- public/instruments/guitar.js
+++ public/instruments/guitar.js
@@ -1,84 +1,41 @@
-// button for testing
-const playBTN = document.getElementById("play-btn")
-
-// midi note for testing
-let midiNote = 45;
-
-// string pitch for testing
-let stringPitch = 110;
-
-class GuitarString{
-    constructor(stringFundemental){
-        this.stringFundemental = stringFundemental; 
-    }
-    
-    fundementalFrequency = new Tone.LowpassCombFilter({
-        delayTime: this.stringFundemental,
-        resonance: 0.2,
-        dampening: 12000
-    }).toDestination();
-
-    delay = new Tone.FeedbackDelay({
-        delayTime: 1/(Tone.mtof(midiNote)),
-        feedback: 0.93,
-        wet: 1
-    }).connect(this.fundementalFrequency);
-
-    noiseFilter = new Tone.Filter({
-        frequency: 1600,
-        type: "lowpass",
-        rolloff: -24,
-    }).connect(this.delay);
-
-    // noiseEnvelope = new Tone.AmplitudeEnvelope({
-    //     attack: 0.001,
-    //     decay: 0.054,
-    //     sustain: 0.0,
-    //     release: 0.002
-    // }).toDestination();
-
-    exciter = new Tone.NoiseSynth({
-        // envelope: this.noiseEnvelope,
-        noise: "white",
-    }).connect(this.noiseFilter);
-
-    Pluck(note, strumDelay){
-        this.delayTime = 1/Tone.mtof(note);
-        this.exciter.triggerAttackRelease(0.1, Tone.now()+(strumDelay/1000));
-    }
-}
-
-class Guitar{
-    guitarStringLowE = new GuitarString(0.0121);
-    guitarStringA = new GuitarString(0.0091)
-    guitarStringD = new GuitarString(0.0068);
-    guitarStringG = new GuitarString(0.0051);
-    guitarStringB = new GuitarString(0.004);
-    guitarStringHighE = new GuitarString(0.003);
-    
-    allStrings = [this.guitarStringLowE, this.guitarStringA, this.guitarStringD, 
-        this.guitarStringD, this.guitarStringG, this.guitarStringB, this.guitarStringHighE]
-
-    DownStrum(strumSpeed){
-        for (let string = 0; string < this.allStrings.length; string++){
-            this.allStrings[string].Pluck((45),(strumSpeed*string))
-        }
-    }
-}
-
-// const guitarString_1 = new GuitarString()
-const guitar = new Guitar()
-
-playBTN.addEventListener("click", async () => {
-	if (Tone.Context.state !== "running"){
-        Tone.start();
-    }
-    // guitar.toFrequency(midiNote, "midi");
-    // guitarString_1.exciter.triggerAttackRelease('8n');
-    // guitar.guitarStringB.exciter.triggerAttackRelease('8n');
-    guitar.DownStrum(40);
-});
-
-
-
-
+const guitarStringLowE = new Tone.Sampler({
+     urls: {
+        'A2': "a2.ogg",
+        'A#2': "as2.ogg",
+        'B2': "b2.ogg",
+        'E2': "e2.ogg",
+        'F2': "f2.ogg",
+        'F#2': "fs2.ogg",
+        'G2': "g2.ogg",
+        'G#2': "gs2.ogg",
+        'A3': "a3.ogg",
+        'A#3': "as3.ogg",
+        'C3': "c3.ogg",
+        'C#3': "cs3.ogg",
+        'D3': "d3.ogg",
+        'D#3': "ds3.ogg",
+        'E3': "e3.ogg",
+        'F3': "f3.ogg",
+        'F#3': "fs3.ogg",
+        'G3': "g3.ogg",
+        'G#3': "gs3.ogg",
+        'A4': "a4.ogg",
+        'A#4': "as4.ogg",
+        'B4': "b4.ogg",
+        'C4': "c4.ogg",
+        'C#4': "cs4.ogg",
+        'D4': "d4.ogg",
+        'D#4': "ds4.ogg",
+        'E4': "e4.ogg",
+        'F4': "f4.ogg",
+        'F#4': "fs4.ogg",
+        'G4': "g4.ogg",
+        'G#4': "gs4.ogg",
+        'D5': "d5.ogg",
+        'D#5': "ds5.ogg",
+        'E5': "e5.ogg",
+        'G5': "g5.ogg",
+        'G#5': "gs5.ogg",
+    },
+    baseUrl: "/samples/guitar/",
+})
